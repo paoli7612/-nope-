3cacc5f916c4cbbca41bb546904627e1d969fab5
paoli7612 <paoli7612@gmail.com>
2020-10-11 15:03:45 +0200

    full screen

--- src/main.lua
+++ src/main.lua
@@ -2,6 +2,7 @@ local Boss = require('boss')
 
 function love.load()
     boss = Boss()
+    love.window.setMode(800, 600, {fullscreen=true})
 end
 
 function love.update(dt)
@@ -9,17 +10,23 @@ function love.update(dt)
 end
 
 function love.draw()
+    love.graphics.print("ESC to quit", 10, 10)
     boss.player.draw()
 end
 
-function love.keypressed(key) end
-function love.mousepressed(x, y, button) end
-function love.mousereleased(x, y, button) end
-function love.focus(f) end
-function love.quit() end
+function love.keypressed(key)
+    if key == 'escape' then
+        love.event.quit(0)
+    end
+end
 function love.keyreleased(key)
     if key == 'up' then boss.player.dy = 0 end
     if key == 'down' then boss.player.dy = 0 end
     if key == 'left' then boss.player.dx = 0 end
     if key == 'right' then boss.player.dx = 0 end
 end
+
+function love.mousepressed(x, y, button) end
+function love.mousereleased(x, y, button) end
+function love.focus(f) end
+function love.quit() end
