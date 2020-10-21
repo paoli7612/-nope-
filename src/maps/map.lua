7eab93b673bc3f4ecaaa6ec62422dfc74003c21f
paoli7612 <paoli7612@gmail.com>
2020-10-21 15:34:16 +0200

    spawn map

--- src/maps/map.lua
+++ src/maps/map.lua
@@ -1,11 +1,18 @@
 local Group = require('sprites/group')
 
+local Class = {
+    decor = require('sprites/decor'),
+    npc = require('sprites/npc')
+}
+
 function Map(boss, name)
     local map = {}
-
-    local file = require('maps/data/'..name)
-
     boss.group = Group(boss)
+    local file = require('maps/data/'..name)
+    for i,sprite in ipairs(file) do
+        sprite = Class[sprite.c](boss, sprite.x, sprite.y, sprite.q)
+        boss.group.add(sprite)
+    end
 
     return map
 end
