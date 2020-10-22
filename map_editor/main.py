f426e569eb9bc24c26c50870b9bac755ea2963b1
paoli7612 <paoli7612@gmail.com>
2020-10-22 19:26:27 +0200

    piazzo di precisione

--- map_editor/main.py
+++ map_editor/main.py
@@ -10,7 +10,11 @@ class Game:
             self.sprites = pygame.sprite.Group()
             self.selected = None
             load(self, 'spawn')
+
+            self.t = [0,0] # trasla freccie
+
             self.loop()
+
 
     def loop(self):
         self.running = True
@@ -35,12 +39,25 @@ class Game:
                         for sprite in self.sprites:
                             if pygame.sprite.collide_rect(sprite, pos):
                                 self.selected = sprite
+                                self.t = list(sprite.rect.center)
                                 break;
+                elif event.type == pygame.MOUSEMOTION:
+                    if self.selected:
+                        self.selected.rect.center = pygame.mouse.get_pos()
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_UP:
+                        self.t[1] -= 1
+                    elif event.key == pygame.K_DOWN:
+                        self.t[1] += 1
+                    elif event.key == pygame.K_LEFT:
+                        self.t[0] -= 1
+                    elif event.key == pygame.K_RIGHT:
+                        self.t[0] += 1
+                    self.selected.rect.center = self.t
+
 
     def update(self):
         self.sprites.update()
-        if self.selected:
-            self.selected.rect.center = pygame.mouse.get_pos()
 
     def draw(self):
         self.screen.fill((255, 255, 255))
