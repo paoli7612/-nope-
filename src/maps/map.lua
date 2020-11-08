7d33f2c51c4857571fedbfb0156293b8c85385bf
paoli7612 <paoli7612@gmail.com>
2020-11-08 14:43:30 +0100

    layered

--- src/maps/map.lua
+++ src/maps/map.lua
@@ -1,10 +1,6 @@
 local Group = require('sprites/group')
-
-local Class = {
-    decor = require('sprites/decor'),
-    npc = require('sprites/npc'),
-    floor = require('sprites/floor')
-}
+local Decor = require('sprites/decor')
+local Nps = require('sprites/npc')
 
 function Map(boss, name)
     local map = {}
@@ -16,7 +12,7 @@ function Map(boss, name)
         qx = love.math.random(4)
         qy = love.math.random(4)
         x, y = boss.settings.pixels(npc.x, npc.y)
-        sprite = Class.npc(boss, map.group, x, y, {qx, qy}, npc.l)
+        sprite = Npc(boss, map.group, x, y, {qx, qy}, npc.l)
     end
 
     -- TILES --
@@ -24,14 +20,14 @@ function Map(boss, name)
         for x=tiles.s[1], tiles.e[1] do
             for y=tiles.s[2], tiles.e[2] do
                 local px, py = boss.settings.pixels(x, y)
-                Class[(tiles.n or 'decor')](boss, map.group, px, py, tiles.q, tiles.c , tiles.l or 2)
+                Decor(boss, map.group, px, py, tiles.q, tiles.c , tiles.l or 2)
             end
         end
     end
 
     for i, tile in ipairs(file.tile) do
         local px, py = boss.settings.pixels(tile.x, tile.y)
-        Class[(tile.n or 'decor')](boss, map.group, px, py, tile.q, tile.c, tile.l or 2)
+        Decor(boss, map.group, px, py, tile.q, tile.c, tile.l or 2)
     end
 
     function map.load()
